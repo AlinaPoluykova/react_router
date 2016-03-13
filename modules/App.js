ce75e91a7095446e2f04951a6bb7d502c895449e
Alina <alina_poluikowa@mail.ru>
2016-03-13 13:58:35 +0300

    Router 4: nav links wrapper + active status

--- modules/App.js
+++ modules/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router'
+//import { Link } from 'react-router'
+import NavLink from './NavLink'
 
-export default class App extends React.Component {
+export default class App extends React.Component{
     render() {
         return (
             <div>
-                <h1>Nested links</h1>pwd
+                <h1>Nested links</h1>
                 <ul role="nav">
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/repos">Repos</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/repos">Repos</NavLink></li>
                 </ul>
                 {this.props.children}
             </div>
         );
     }
 }
+
+
 
