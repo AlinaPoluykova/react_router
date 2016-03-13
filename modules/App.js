791b589eadff79cfb53e43eaa049110cd7e86f16
Alina <alina_poluikowa@mail.ru>
2016-03-13 13:08:48 +0300

    Router 2: adding links

--- modules/App.js
+++ modules/App.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 export default class App extends React.Component{
   render() {
-    return (<div>Hello!</div>);
-  }
+    return (
+        <div>
+          <h1>React Router Tutorial</h1>
+          <ul role="nav">
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/repos">Repos</Link></li>
+          </ul>
+        </div>
+    );
 }
 
