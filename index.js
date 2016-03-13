764812691d57790d9df93ef89695b5fedeb10f60
Alina <alina_poluikowa@mail.ru>
2016-03-13 23:08:18 +0300

    Router 5: params

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
+import Repo from './modules/Repo'
 
 render(
     (
-        <Router history={hashHistory}>
-            <Route path="/" component={App} >
-                <Route path="/repos" component={Repos} />
-                <Route path="/about" component={About} />
+        <Router>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+                <Route path="/repos" component={Repos}/>
+                {/* add the new route */}
+                <Route path="/repos/:userName/:repoName" component={Repo}/>
+                <Route path="/about" component={About}/>
             </Route>
         </Router>
 
