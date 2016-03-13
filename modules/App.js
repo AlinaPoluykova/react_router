2b9cb56cdcdcff768e69504fd25d2c1694c9c635
Alina <alina_poluikowa@mail.ru>
2016-03-13 13:25:20 +0300

    Router 3: nested links

--- modules/App.js
+++ modules/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-export default class App extends React.Component{
-  render() {
-    return (
-        <div>
-          <h1>React Router Tutorial</h1>
-          <ul role="nav">
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/repos">Repos</Link></li>
-          </ul>
-        </div>
-    );
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Nested links</h1>pwd
+                <ul role="nav">
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/repos">Repos</Link></li>
+                </ul>
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
